835cbb89970e21ca609fbb7994c3338cc106b2cd
scamisay <scamisay@candoit.com.ar>
2018-03-05 04:02:05 -0300

    -primera version del index cell method

--- src/ar/edu/itba/sds/domain/IndexCell.java
+++ src/ar/edu/itba/sds/domain/IndexCell.java
@@ -1,9 +1,6 @@
 package ar.edu.itba.sds.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
@@ -14,7 +11,6 @@ public class IndexCell {
     private Double rc;
     private List<Particle> particles = new ArrayList<>();
     private Cell[][] environment;
-    private Map<Particle, List<Particle>> output = new HashMap<>();
 
     public IndexCell(Integer m, Double l, Double rc, List<Particle> particles) {
         M = m;
@@ -52,11 +48,30 @@ public class IndexCell {
         }
 
         //todo: definir vecinos para cada celda
+        for (Integer x = 0; x < M; x++) {
+            for (Integer y = 0; y < M; y++) {
+                environment[x][y].setNeigbours(calculateParticleNeighbours(x,y));
+            }
+        }
     }
 
-    public void calculate(){
-        output = particles.stream()
-                .collect(Collectors.toMap(p->p, this::calculateNeighbours));
+    public Map<Particle, List<Particle>> calculate(){
+        return particles.stream()
+                .collect(Collectors.toMap(p->p, this::calculateParticleNeighbours));
+    }
+
+    /***
+     * (x,y+1) (x+1,y+1)
+     * (x,y)   (x+1,y)
+     *         (x+1,y-1)
+     **/
+    private List<Cell> calculateParticleNeighbours(Integer x, Integer y){
+        return Arrays.asList(
+                environment[x][(y+1)%M],
+                environment[(x+1)%M][(y+1)%M],
+                environment[(x+1)%M][y],
+                environment[(x+1)%M][((y-1)%M)<0?((y-1)%M)+M:(y-1)%M]
+        );
     }
 
     /**
@@ -64,11 +79,13 @@ public class IndexCell {
      * por cada celda traer particulas
      * filtrar las particulas que esten a menos de rc de la particle
      */
-    private List<Particle> calculateNeighbours(Particle particle){
+    private List<Particle> calculateParticleNeighbours(Particle particle){
         return particle.getCell().getNeighbours().stream()
                 .map(Cell::getParticles)
                 .flatMap(List::stream)
                 .filter(p -> particle.isCloseEnough(p, rc))
                 .collect(Collectors.toList());
     }
+
+
 }
