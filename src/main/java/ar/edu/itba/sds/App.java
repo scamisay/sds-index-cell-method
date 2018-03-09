966f54a8a981a9fbefc0299e67e9ed58d8fce47e
scamisay <scamisay@candoit.com.ar>
2018-03-09 03:49:44 -0300

    deployable
    mvn clean install
    mvn exec:java

--- src/main/java/ar/edu/itba/sds/App.java
+++ src/main/java/ar/edu/itba/sds/App.java
@@ -1,28 +1,25 @@
 package ar.edu.itba.sds;
 
-import ar.edu.itba.sds.domain.CellIndexMethod;
 import ar.edu.itba.sds.domain.Particle;
-import ar.edu.itba.sds.helper.MethodComparator;
 import ar.edu.itba.sds.helper.ParticleGenerator;
 import ar.edu.itba.sds.helper.Printer;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class Main {
-
+/**
+ * Hello world!
+ *
+ */
+public class App {
     private static final Double RADIX = .1;
 
     public static void main(String[] args) {
-        Integer N,M;
-        Double L;
+        Integer N, M;
+        Double L, rc;
         boolean periodicContourCondition;
-        while(true){
-            try{
+        while (true) {
+            try {
                 Scanner sc = new Scanner(System.in);
                 System.out.print("N:");
                 N = sc.nextInt();
@@ -33,10 +30,13 @@ public class Main {
                 System.out.print("M:");
                 M = sc.nextInt();
 
+                System.out.print("rc:");
+                rc = sc.nextDouble();
+
                 System.out.print("periodicContourCondition:");
                 periodicContourCondition = sc.nextBoolean();
                 break;
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Vuelva a intentar introducir los valores");
             }
         }
@@ -46,15 +46,9 @@ public class Main {
         Integer M = 2;
         boolean periodicContourCondition = false;
 */
-        List<Particle> particles = new ParticleGenerator().generate(N,L,RADIX);
-        Printer printer = new Printer(particles,L, M,2.,periodicContourCondition,0);
+        List<Particle> particles = new ParticleGenerator().generate(N, L, RADIX);
+        Printer printer = new Printer(particles, L, M, rc, periodicContourCondition, 0);
         printer.printFiles();
 
     }
-
-
-
-
-
-
 }
